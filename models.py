57b158ef6625415e14e7d2ea07c89770fd8cb774
Jonathan Lindén <jonathan@jlinden.se>
2021-05-29 11:21:17 +0200

    Set up a basic model.

---
+++ models.py
@@ -0,0 +1,31 @@
+from torch import nn
+
+
+class ConvNeuralNetwork(nn.Module):
+    def __init__(self):
+        super(ConvNeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.conv_network = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=8, kernel_size=(3, 3),
+                      stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3),
+                      stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3),
+                      stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3),
+                      stride=(1, 1), padding=(1, 1)),
+            nn.ReLU()
+        )
+
+        self.fc = nn.Linear(in_features=12800*2, out_features=2)
+
+    def forward(self, x):
+        x = self.conv_network(x)
+        x = x.view(-1, 12800*2)
+        x = self.fc(x)
+        return x
