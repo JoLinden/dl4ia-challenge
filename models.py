c3d56df6f6e8177800c6aab6ebbf5c4180ab81c2
Hannes Kuchelmeister <13hannes11@users.noreply.github.com>
2021-05-31 15:47:32 +0200

    add residual convolutional net

--- models.py
+++ models.py
@@ -62,3 +62,59 @@ class ConvNeuralNetwork(nn.Module):
         x = x.view(-1, 12800*2)
         x = self.fc(x)
         return x
+
+    
+class ResConvNeuralNetwork(nn.Module):
+    def __init__(self):
+        super(ConvNeuralNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.conv_network = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=8, kernel_size=(3, 3),
+                      stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Dropout(),            
+            ResidualBlock(layer_count=3, channel_num=8),
+            nn.Dropout(),
+            ResidualBlock(layer_count=3, channel_num=8),
+            nn.Dropout(),
+            ResidualBlock(layer_count=3, channel_num=8),
+            
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3),
+                      stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Dropout(),
+            ResidualBlock(layer_count=3, channel_num=16),
+            nn.Dropout(),
+            ResidualBlock(layer_count=3, channel_num=16),
+            nn.Dropout(),
+            ResidualBlock(layer_count=3, channel_num=16),
+            
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3),
+                      stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Dropout(),
+            ResidualBlock(layer_count=3, channel_num=32),
+            nn.Dropout(),
+            ResidualBlock(layer_count=3, channel_num=32),
+            nn.Dropout(),
+            ResidualBlock(layer_count=3, channel_num=32),
+            
+            nn.Dropout(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3),
+                      stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.Dropout(),
+            ResidualBlock(layer_count=3, channel_num=64),
+            nn.Dropout(),
+            ResidualBlock(layer_count=3, channel_num=64),
+            nn.Dropout(),
+            ResidualBlock(layer_count=3, channel_num=64),
+        )
+        
+        self.classification = ConvolutionalClassification(2)
+    def forward(self, x):
+        x = self.conv_network(x)
+        x = self.classification(x)
+        return x
